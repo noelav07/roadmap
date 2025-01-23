931e421b43f3ff12ef31b4332ad7d13f6ff62dff
noelav07 <noel.a.v.07@gmail.com>
2025-01-23 15:17:41 +0530

    finally the roadgen is working

--- main.py
+++ main.py
@@ -2,36 +2,36 @@ import streamlit as st
 
 home = st.Page(
     "crops/home.py",
-    title="About Me",
-    icon=":material/account_circle:",
+    title="Home",
+    icon=":material/home:",
 #    default=True,  # Default page
 )
 roadmap_generator = st.Page(
     # "crops/roadgen.py",
-    "crops/roadgen.py",
-    title="Roadmap Generator",
-    icon=":material/bar_chart:",
+    "crops/roadgen_v2.py",
+    title="Roadmap Guide",
+    icon=":material/road:",
 )
 chat = st.Page(
     "crops/chat.py",
-    title="Chat Bot",
-    icon=":material/smart_toy:",
+    title="Career Assistant",
+    icon=":material/robot:",
 )
 popular = st.Page(
     "crops/popular_roadmaps.py",
-    title="Popular",
-    icon=":material/smart_toy:",
+    title="Popular Roadmaps",
+    icon=":material/map:",
 )
 
 pg = st.navigation(
     {
         "Info": [home],
-        "Projects": [roadmap_generator, chat, popular],
+        "Sections": [roadmap_generator, chat, popular],
     }
 )
 
 # --- SHARED ON ALL PAGES ---
-st.sidebar.markdown("Noel")
+st.sidebar.markdown("🗂️Career Assistance ")
 
 # --- RUN NAVIGATION ---
 pg.run()
